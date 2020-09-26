6795143710b1c26a6bbfeda2866aefed9539695f
Katie Kat Martin <ghosttk@protonmail.com>
2020-09-26 13:16:00 +0100

    basic project structure

---
+++ WebCam/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {WebcamComponent} from "./webcam/webcam.component";
+
+
+const routes: Routes = [
+  {path: '',component:WebcamComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
